e0ad658d036808cec53aea034396a2f98ad5c2c3
Jahirul Islam <48243999+Jahirul10@users.noreply.github.com>
2022-05-20 11:14:38 +0600

    Add files via upload

---
+++ B. Z mod X = C.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+     int a=0,b=0,c=0;
+     cin>>a>>b>>c;
+     cout<<a+b+c<<" "<<c+b<<" "<<c<<endl;
+    }
+    return 0;
+}
